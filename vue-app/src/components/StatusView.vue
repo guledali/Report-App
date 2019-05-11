e3731f40a4578e8b0770c2f50d14feeeb801f237
guledali <guled.ali01@gmail.com>
2019-05-11 18:10:15 +0200

    Initital styling for status-view

---
+++ vue-app/src/components/StatusView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-secondary vh-100 vw-100">
+    <h2 class="pt-5 text-light text-center">Morning status</h2>
+    <ul
+        class="list-group w-50 mx-auto"
+      >
+        <li class="list-group-item border border-3-green mt-2">
+          <span class="lead font-weight-bold">Adam</span
+          ><i class="fas fa-check  ml-2  fa-2x text-success"></i>
+        </li>
+      </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .border-3-red {
+      border: 3px solid #dc3545 !important;
+    }
+    .border-3-green {
+      border: 3px solid #28a745 !important;
+    }
+
+</style>
---
+++ vue-app/src/views/Status.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <StatusView />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import StatusView from '@/components/StatusView.vue'
+
+export default {
+  name: 'status',
+  components: {
+    StatusView
+  }
+}
+</script>
