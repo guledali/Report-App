9d17487c82eeb41df4f64dc347717d8ce0222f3c
guledali <guled.ali01@gmail.com>
2019-05-25 13:45:48 +0200

    Revert "Feature node"

--- vue-app/src/router.js
+++ vue-app/src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Report from './views/Report.vue'
-import Status from './views/Status.vue'
+import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -11,13 +10,16 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'report',
-      component: Report
+      name: 'home',
+      component: Home
     },
     {
-      path: '/status',
-      name: 'status',
-      component: Status
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
